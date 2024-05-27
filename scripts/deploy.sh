e6d74ffedfbbd9844e1e4dfa9f95dae07292e640
peterajessup <peterajessup@users.noreply.github.com>
2024-05-27 12:00:56 +1000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,7 @@ oc delete QueueManager mq1
 
 # Delete the route object and secret for the QueueManager keystore (if any), and the mqsc configMap
 oc delete route mq1route
+oc delete route mq1routetlss
 oc delete route mq1routercvr
 oc delete secret mq1key
 oc delete secret tls-eris-trust
@@ -15,6 +16,7 @@ oc delete configMap mq1-mqsc
 set -e
 # Create the route and the keystore secret and mqsc configMap
 oc apply -f mq1Route.yaml
+oc apply -f mq1routetlss.yaml
 oc apply -f routeMQ1rcvr.yaml
 oc create secret tls mq1key --cert=./tls/tls.crt --key=./tls/tls.key
 oc create secret generic tls-eris-trust --from-file ./tls/CSQ9.crt
