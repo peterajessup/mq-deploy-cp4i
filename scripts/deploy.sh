651424b2c435bfa644e2e0de013a9269914d97c1
peterajessup <peterajessup@users.noreply.github.com>
2025-02-28 15:21:23 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,21 +6,26 @@ set +e
 oc delete QueueManager sydqm melqm
 
 # Delete the route object and secret for the QueueManager keystore (if any), and the mqsc configMap
-oc delete route mq1route
-oc delete route mq1routetlss
-oc delete route mq1routercvr
+oc delete route sydroute
+oc delete route melroute
+
 oc delete secret mq1key
-oc delete secret tls-eris-trust
+
 oc delete configMap mq1-mqsc
 # oc delete persistentvolumeclaim data-mq1-ibm-mq-0 data-mq1-ibm-mq-1 data-mq1-ibm-mq-2
 set -e
 # Create the route and the keystore secret and mqsc configMap
-oc apply -f mq1Route.yaml
-oc apply -f mq1routetlss.yaml
-oc apply -f routeMQ1rcvr.yaml
-oc create secret tls mq1key --cert=./tls/tls.crt --key=./tls/tls.key
-oc create secret generic tls-eris-trust --from-file ./tls/CSQ9.crt
+oc apply -f sydRoute.yaml
+
+oc create secret tls mqkey --cert=./tls/tls.crt --key=./tls/tls.key
+# ----for CRR
+oc create secret tls melkey --cert=./tls.crt --key=./tls/tls.key
+oc create secret tls sydkey --cert=./tls.crt --key=./tls/tls.key
+oc create secret generic tls-mel --from-file ./tls/tls.crt 
+oc create secret generic tls-syd --from-file ./tls/tls.crt
+#------------
 oc create -f mqsc/mqsc.yaml
 
 set -e
-oc apply -f mqDeploy.yaml
+oc apply -f sydCrr.yaml
+oc apply -f melCrr.yaml
