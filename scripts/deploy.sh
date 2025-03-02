8c61d62ea4e02a534ac9f3db7ef85258331404e5
peterajessup <peterajessup@users.noreply.github.com>
2025-03-03 09:21:16 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
-oc project mq
+
 set +e
-# Remove the runnning queue manager instance (if any)
 
-oc delete QueueManager sydqm melqm
-
-# Delete the route object and secret for the QueueManager keystore (if any), and the mqsc configMap
-oc delete route sydroute melroute
-
-oc delete secret mqkey
-
+# --------For the DR site-------------
+oc project mqdr
+oc delete QueueManager melqm
+oc delete secret mqkey melkey tls-mel
 oc delete configMap mq1-mqsc
+oc delete route melroute
+
 set -e
-# Create the route and the keystore secret and mqsc configMap
-oc apply -f sydRoute.yaml
 oc apply -f melRoute.yaml
 
 oc create secret tls mqkey --cert=./tls/tls.crt --key=./tls/tls.key
-# ----for CRR
+# Key and cert for CRR
 oc create secret tls melkey --cert=./tls.crt --key=./tls/tls.key
-oc create secret tls sydkey --cert=./tls.crt --key=./tls/tls.key
 oc create secret generic tls-mel --from-file ./tls/tls.crt 
-oc create secret generic tls-syd --from-file ./tls/tls.crt
-#------------
+
 oc create -f mqsc/mqsc.yaml
+oc apply -f melCrr.yaml
+# ----------------------------------
+
+# --------For the Live site-------------
+set +e
+oc project mq
+
+oc delete QueueManager sydqm 
+oc delete route sydroute 
+oc delete secret mqkey sydkey tls-syd
+oc delete configMap mq1-mqsc
 
 set -e
+oc apply -f sydRoute.yaml
+
+oc create secret tls mqkey --cert=./tls/tls.crt --key=./tls/tls.key
+
+# Key and cert for CRR
+oc create secret tls sydkey --cert=./tls.crt --key=./tls/tls.key
+oc create secret generic tls-syd --from-file ./tls/tls.crt
+
+oc create -f mqsc/mqsc.yaml
+
+
 oc apply -f sydCrr.yaml
-oc apply -f melCrr.yaml
+
